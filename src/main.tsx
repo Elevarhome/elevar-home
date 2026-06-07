1f0b4207e61cc37f1ff3308b4817c1b816bf805b
Elevarhome <contato.elevar26@gmail.com>
2026-06-06 21:04:23 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home'
+import LightBar from './pages/LightBar'
+
+// Roteamento simples por caminho
+const path = window.location.pathname;
+const Page = path === '/light-bar' ? LightBar : Home;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <Page />
   </StrictMode>,
 )
